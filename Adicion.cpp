ba96d507b9f2a34ea4f8acdc4ffa5725038958c8
GermanVarona <varona.german@yahoo.com>
2017-04-05 00:42:41 -0300

    Add files via upload

---
+++ Adicion.cpp
@@ -0,0 +1,21 @@
+/* TP#1 Adicion
+German O Varona
+K1051
+20170404
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include<conio.h>
+
+int main()
+{
+	int a, b, c;
+	std::cin >> a;
+	std::cin >> b;
+	c = a + b;
+	std::cout << c;
+	getch();
+	return 0;
+}
+
